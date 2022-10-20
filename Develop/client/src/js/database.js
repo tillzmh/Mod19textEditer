a7b9a1a1879898d9fe677dd534f765c1183e2b84
tillzmh <tillzmh@users.noreply.github.com>
2022-10-20 17:10:53 -0400

    GET all logic created that gets content from db

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -17,7 +17,7 @@ export const putDb = async (content) => {
   console.log('add to database');
   
   // create a connection to the data bases and version
-  const jateDb = await openDB('jate', 1, );
+  const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite'); // created a new transaction and specify the db and db privileges
   const store = tx.objectStore('jate'); // opened the desired object store
   const request = store.add({content}); // used .add method on the stroe and pass in the content
@@ -26,6 +26,15 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from database');
+  const jateDb = await openDB('jate', 1); // created a new transaction and specify the db and db privileges
+  const tx = jateDb.transaction('jate', 'readonly'); 
+  const store = tx.objectStore('jate'); // opened the desired object store 
+  const request = store.get(1); // getAll method to get all data in the db
+  const result = await request; 
+  console.log ('result.value',result);
+  return result?.value;
+};
 
 initdb();
