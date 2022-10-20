38e8584281b94a66f2577917e05aa5f00086e424
tillzmh <tillzmh@users.noreply.github.com>
2022-10-20 16:53:24 -0400

    create a connection to the DB

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,7 +13,12 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('add to database');
+  
+  // create a connection to the data bases and version
+  const jateDb = await openDB('jate', 1, );
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
