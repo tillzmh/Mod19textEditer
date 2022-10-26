45cf4f74f12a5a5f5a4b405dff700120e81ea38d
tillzmh <tillzmh@users.noreply.github.com>
2022-10-26 19:45:42 -0400

    installed packages, edited webpack config folder

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -22,10 +22,51 @@ module.exports = () => {
         template: './index.html',
         title: 'Mod19textEditor',
       }),
-
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "TextEditor",
+        short_name: "JATE",
+        description: "",
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        crossorigin: 'use-credentials',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      })
+    ],
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            }
+          }
+        }
       ],
     },
   };
